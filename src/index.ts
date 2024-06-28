dfce9fd9a33ea6ce78461e54b351b77429373dd6
Jupi <spacejupiter0@gmail.com>
2024-06-28 16:55:55 +0300

    created the basic app

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,21 @@
 import express from 'express';
 import cors from "cors";
 import 'dotenv/config';
-import apiRoutes from "./API/Routes/mainRoutes";
+import apiRoutes from "./API/Routes/apiRoutesWithVersions";
+import { events } from './Database';
+import helmet from 'helmet';
 
+export const eventsDb = new events();
 const app = express();
 const PORT = Number(process.env.PORT) || 3005;
 
+
+app.use(helmet()); // helmet for header securing and making sure our api is safe.
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routing
+// Routes
 app.use(apiRoutes);
 
 app.all("*", (_, res) => res.status(404).send("This route was not found."));
